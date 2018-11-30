cd20777e6820379cc634c295164ce1f958ab07b7
Ramona Walls <rlwalls2008@gmail.com>
2018-11-29 20:06:50 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'YOUR QUICKSTART NAME HERE as topic_quick_start'
+project = 'Group Project Quickstart'
 copyright = '2018, CyVerse'
-author = 'Your Name'
+author = 'Ramona L. Walls'
 version = '1.0'
 release = '1.0'
 
